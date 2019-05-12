75c7dc766c16942134192ee3041cff33c040285c
Callan Milne <callan.milne@eviratec.com.au>
2019-05-12 17:39:41 +1000

    build: v1.0.0-rc2

--- dist/app.js
+++ dist/app.js
@@ -616,196 +616,6 @@ angular.module('TaskEvoWebui.UserDashboard').config(['$stateProvider', function 
 }]);
 'use strict';
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-/**
- * Copyright (c) 2019 Callan Peter Milne
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
-
-angular.module('TaskEvoWebui.ListPage').controller('ListPageController', ListPageController);
-
-ListPageController.$inject = ['$api', '$scope', '$state', '$mdDialog', '$timeout', 'list'];
-function ListPageController($api, $scope, $state, $mdDialog, $timeout, list) {
-
-  var $listPage = this;
-
-  var completedItems = [];
-  var todoItems = [];
-
-  $listPage.list = list;
-
-  $listPage.completedItems = completedItems;
-  $listPage.todoItems = todoItems;
-
-  initItemLists();
-
-  $listPage.navToParent = function ($event) {
-    navToParent();
-  };
-
-  $listPage.toggleComplete = function (list) {
-    var isCompleted = null !== list.Completed;
-    if (isCompleted) {
-      setItemNotComplete(list);
-      return;
-    }
-
-    setItemComplete(list);
-  };
-
-  $listPage.createItem = function ($event) {
-
-    var confirm = $mdDialog.prompt().title('Name your new item').placeholder('Do ...').ariaLabel('Item title').initialValue('').targetEvent($event).ok('Create Item').cancel('Cancel');
-
-    $mdDialog.show(confirm).then(function (result) {
-      createItem(result);
-    }, function () {});
-  };
-
-  $listPage.deleteList = function ($event) {
-
-    var confirm = $mdDialog.confirm().title('Are you sure?').textContent('This will permanently delete your list: ' + list.Title).ariaLabel('Delete list').targetEvent($event).ok('Delete List').cancel('Cancel');
-
-    $mdDialog.show(confirm).then(function () {
-      deleteList(list.Id);
-    }, function () {
-      // do nothing
-    });
-  };
-
-  $listPage.renameList = function ($event) {
-
-    var confirm = $mdDialog.prompt().title('Rename List').placeholder(list.Title).ariaLabel('List title').initialValue(list.Title).targetEvent($event).ok('Save').cancel('Cancel');
-
-    $mdDialog.show(confirm).then(function (newValue) {
-      renameList(list.Id, newValue);
-    }, function () {
-      // do nothing
-    });
-  };
-
-  function renameList(listId, newValue) {
-    if (list.Id !== listId) {
-      return;
-    }
-
-    $api.apiPutNewValue('/list/' + listId + '/title', newValue).then(function (res) {
-      updateListTitle(newValue);
-    }).catch(function (err) {
-      console.log(err);
-      notifyRenameListError();
-    });
-  }
-
-  function updateListTitle(newValue) {
-    $scope.$apply(function () {
-      list.Title = newValue;
-    });
-  }
-
-  function deleteList(listId) {
-    if (list.Id !== listId) {
-      return;
-    }
-
-    $api.apiDelete('/list/' + listId).then(function (res) {
-      navToParent();
-    }).catch(function (err) {
-      console.log(err);
-      notifyDeleteListError();
-    });
-  }
-
-  function notifyDeleteListError() {
-    $mdDialog.show($mdDialog.alert().title('Error').textContent('An unexpected error was encountered while deleting the list.').ariaLabel('Error notification').ok('Ok'));
-  }
-
-  function notifyRenameListError() {
-    $mdDialog.show($mdDialog.alert().title('Error').textContent('An unexpected error was encountered while renaming the list.').ariaLabel('Error notification').ok('Ok'));
-  }
-
-  function navToParent() {
-    var hasParentId = null !== list.ParentId;
-    if (hasParentId) {
-      $state.go('app.user.listPage', { listId: list.ParentId });
-      return;
-    }
-
-    $state.go('app.user.categoryPage', { categoryId: list.CategoryId });
-  }
-
-  function initItemLists() {
-    var allLists = $listPage.list.Lists.slice();
-
-    completedItems.push.apply(completedItems, _toConsumableArray(allLists.filter(function (list) {
-      return 'Completed' in list && list.Completed !== null;
-    })));
-
-    todoItems.push.apply(todoItems, _toConsumableArray(allLists.filter(function (list) {
-      return !('Completed' in list) || list.Completed === null;
-    })));
-  }
-
-  function setItemNotComplete(item) {
-    $api.apiPutNewValue('/list/' + item.Id + '/completed', null).then(function (res) {
-      $timeout(function () {
-        var i = completedItems.indexOf(item);
-        item.Completed = null;
-        completedItems.splice(i, 1);
-        todoItems.push(item);
-      });
-    }).catch(function (err) {
-      console.log(err);
-    });
-  }
-
-  function setItemComplete(item) {
-    $api.apiPutNewValue('/list/' + item.Id + '/completed', 'now').then(function (res) {
-      $timeout(function () {
-        var i = todoItems.indexOf(item);
-        item.Completed = Math.floor(Date.now() / 1000);
-        todoItems.splice(i, 1);
-        completedItems.push(item);
-      });
-    }).catch(function (err) {
-      console.log(err);
-    });
-  }
-
-  function createItem(title) {
-
-    var newList = {
-      ParentId: list.Id,
-      Title: title,
-      Completed: null
-    };
-
-    $api.apiPost('/lists', newList).then(function (res) {
-      $timeout(function () {
-        Object.assign(newList, res.data);
-        newList.Id = res.data.Id;
-      });
-    }).catch(function (err) {
-      console.log(err);
-    });
-
-    todoItems.push(newList);
-  }
-};
-'use strict';
-
 /**
  * Copyright (c) 2019 Callan Peter Milne
  *
@@ -1117,6 +927,221 @@ function CategoryPageController($api, $scope, $state, $mdDialog, $timeout, categ
 };
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+/**
+ * Copyright (c) 2019 Callan Peter Milne
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+angular.module('TaskEvoWebui.ListPage').controller('ListPageController', ListPageController);
+
+ListPageController.$inject = ['$api', '$scope', '$state', '$mdDialog', '$timeout', 'list'];
+function ListPageController($api, $scope, $state, $mdDialog, $timeout, list) {
+
+  var $listPage = this;
+
+  var completedItems = [];
+  var todoItems = [];
+
+  $listPage.list = list;
+
+  $listPage.completedItems = completedItems;
+  $listPage.todoItems = todoItems;
+
+  initItemLists();
+
+  $listPage.navToParent = function ($event) {
+    navToParent();
+  };
+
+  $listPage.toggleComplete = function (list) {
+    var isCompleted = null !== list.Completed;
+    if (isCompleted) {
+      setItemNotComplete(list);
+      return;
+    }
+
+    setItemComplete(list);
+  };
+
+  $listPage.createItem = function ($event) {
+
+    var confirm = $mdDialog.prompt().title('Name your new item').placeholder('Do ...').ariaLabel('Item title').initialValue('').targetEvent($event).ok('Create Item').cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function (result) {
+      createItem(result);
+    }, function () {});
+  };
+
+  $listPage.deleteList = function ($event) {
+
+    var confirm = $mdDialog.confirm().title('Are you sure?').textContent('This will permanently delete your list: ' + list.Title).ariaLabel('Delete list').targetEvent($event).ok('Delete List').cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function () {
+      deleteList(list.Id);
+    }, function () {
+      // do nothing
+    });
+  };
+
+  $listPage.renameList = function ($event) {
+
+    var confirm = $mdDialog.prompt().title('Rename List').placeholder(list.Title).ariaLabel('List title').initialValue(list.Title).targetEvent($event).ok('Save').cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function (newValue) {
+      renameList(list.Id, newValue);
+    }, function () {
+      // do nothing
+    });
+  };
+
+  function renameList(listId, newValue) {
+    if (list.Id !== listId) {
+      return;
+    }
+
+    $api.apiPutNewValue('/list/' + listId + '/title', newValue).then(function (res) {
+      updateListTitle(newValue);
+    }).catch(function (err) {
+      console.log(err);
+      notifyRenameListError();
+    });
+  }
+
+  function updateListTitle(newValue) {
+    $scope.$apply(function () {
+      list.Title = newValue;
+    });
+  }
+
+  function deleteList(listId) {
+    if (list.Id !== listId) {
+      return;
+    }
+
+    $api.apiDelete('/list/' + listId).then(function (res) {
+      navToParent();
+    }).catch(function (err) {
+      console.log(err);
+      notifyDeleteListError();
+    });
+  }
+
+  function notifyDeleteListError() {
+    $mdDialog.show($mdDialog.alert().title('Error').textContent('An unexpected error was encountered while deleting the list.').ariaLabel('Error notification').ok('Ok'));
+  }
+
+  function notifyRenameListError() {
+    $mdDialog.show($mdDialog.alert().title('Error').textContent('An unexpected error was encountered while renaming the list.').ariaLabel('Error notification').ok('Ok'));
+  }
+
+  function navToParent() {
+    var hasParentId = null !== list.ParentId;
+    if (hasParentId) {
+      $state.go('app.user.listPage', { listId: list.ParentId });
+      return;
+    }
+
+    $state.go('app.user.categoryPage', { categoryId: list.CategoryId });
+  }
+
+  function initItemLists() {
+    var allLists = $listPage.list.Lists.slice();
+
+    completedItems.push.apply(completedItems, _toConsumableArray(allLists.filter(function (list) {
+      return 'Completed' in list && list.Completed !== null;
+    })));
+
+    todoItems.push.apply(todoItems, _toConsumableArray(allLists.filter(function (list) {
+      return !('Completed' in list) || list.Completed === null;
+    })));
+  }
+
+  function setItemNotComplete(item) {
+    $api.apiPutNewValue('/list/' + item.Id + '/completed', null).then(function (res) {
+      $timeout(function () {
+        var i = completedItems.indexOf(item);
+        item.Completed = null;
+        completedItems.splice(i, 1);
+        todoItems.push(item);
+      });
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  function setItemComplete(item) {
+    $api.apiPutNewValue('/list/' + item.Id + '/completed', 'now').then(function (res) {
+      $timeout(function () {
+        var i = todoItems.indexOf(item);
+        item.Completed = Math.floor(Date.now() / 1000);
+        todoItems.splice(i, 1);
+        completedItems.push(item);
+      });
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  function createItem(title) {
+
+    var newList = {
+      ParentId: list.Id,
+      Title: title,
+      Completed: null
+    };
+
+    $api.apiPost('/lists', newList).then(function (res) {
+      $timeout(function () {
+        Object.assign(newList, res.data);
+        newList.Id = res.data.Id;
+      });
+    }).catch(function (err) {
+      console.log(err);
+    });
+
+    todoItems.push(newList);
+  }
+};
+'use strict';
+
+/**
+ * Copyright (c) 2019 Callan Peter Milne
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+angular.module('TaskEvoWebui.UserDashboard').controller('DashboardController', DashboardController);
+
+DashboardController.$inject = ['$scope', '$mdDialog', 'userCategories'];
+function DashboardController($scope, $mdDialog, userCategories) {
+
+  $scope.categories = userCategories;
+};
+'use strict';
+
 /**
  * Copyright (c) 2019 Callan Peter Milne
  *
@@ -1290,31 +1315,6 @@ function UserController($api, $scope, $rootScope, $auth, $state, $mdDialog, $tim
       return category.Id === categoryId;
     })[0];
   }
-};
-'use strict';
-
-/**
- * Copyright (c) 2019 Callan Peter Milne
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
-
-angular.module('TaskEvoWebui.UserDashboard').controller('DashboardController', DashboardController);
-
-DashboardController.$inject = ['$scope', '$mdDialog', 'userCategories'];
-function DashboardController($scope, $mdDialog, userCategories) {
-
-  $scope.categories = userCategories;
 };
 'use strict';
 
